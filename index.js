336c4ee11c3456cce979c33ab562e7658a854fcc
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-06-25 13:46:15 -0400

    button to change heading

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('It Works!')
-const button = document.querySelector('button')
+// console.log('It Works!')
+const button = document.querySelector('button');
+
+// to change the heading after pressing button
+function changeHeading() {
+    const changeHeading = document.querySelector('h1');
+    changeHeading.textContent = 'A new Heading!!';
+}
+
+button.addEventListener('click', changeHeading);
