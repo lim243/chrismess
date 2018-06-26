4cbb353551b0817f809c4c7d8c67087c0c7dc2d9
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-06-26 13:34:44 -0400

    Merge my code with day2 code

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+//1st button to change 1st heading
+//2nd button to change 2nd heading
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+
+// to change the heading after pressing button
+function changeFirstHeading() {
+    const changeHeading = document.querySelector('h1');
+    changeHeading.textContent = 'A new Heading!!';
+}
+
+const changeSecondHeading = function() {
+    const changeSecondHeading = document.querySelector('#head2');
+    changeSecondHeading.textContent = 'I am finally free!!';
+}
+
+button1.addEventListener('click', changeFirstHeading);
+button2.addEventListener('click', changeSecondHeading);
+
+//Forms
 const form = document.querySelector('form#flickForm')
 
 const changeHeading = function(ev) {
@@ -15,3 +35,5 @@ const changeHeading = function(ev) {
 }
 
 form.addEventListener('submit', changeHeading)
+
+//REMARKS: document.querySelector() if id then use '#' if class then use '.'
