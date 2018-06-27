73aa8d1e94b60e243d72ebdaf7ef83e70c585d31
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-06-26 21:25:01 -0400

    Split the main function into 3 different functions

--- index.js
+++ index.js
@@ -7,27 +7,32 @@ const addFlickToList = function(ev) {
     if(f.flickName.value == '' || f.flickYear.value == ''){
         alert('Please Do Not Leave Input Field Empty!')
     } else {
-        const flickYear = f.flickYear.value
-        const flickName = f.flickName.value
-
         const item = document.createElement('li')
 
-        let sFlickName = document.createElement('span')
-        sFlickName.setAttribute('class', 'flickName')
-        sFlickName.textContent = flickName + ' '
-        
-        let sFlickYear = document.createElement('span')
-        sFlickYear.setAttribute('class', 'flickYear')
-        sFlickYear.textContent = flickYear
-
-        item.appendChild(sFlickName)
-        item.appendChild(sFlickYear)
+        addFlickNameToItemList(f,item)
+        addFlickYearToItemList(f,item);
 
         const list = document.querySelector('#flicks')
 
         list.appendChild(item)
     }
     f.reset()
+}
+
+function addFlickYearToItemList(f,item){
+    const flickYear = f.flickYear.value;
+    let sFlickYear = document.createElement('span')
+    sFlickYear.setAttribute('class', 'flickYear')
+    sFlickYear.textContent = flickYear
+    item.appendChild(sFlickYear)
+}
+
+function addFlickNameToItemList(f,item){
+    const flickName = f.flickName.value;
+    let sFlickName = document.createElement('span')
+    sFlickName.setAttribute('class', 'flickName')
+    sFlickName.textContent = flickName + ' '
+    item.appendChild(sFlickName)
 }
 
 form.addEventListener('submit', addFlickToList)
