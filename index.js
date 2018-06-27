aaf0b0401189c5f840ed4f4455beb55d3f271297
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-06-27 14:03:41 -0400

    Create a class

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
 
-const app = class App{
+class App{
     constructor(){
         const form = document.querySelector('form#flickForm')
-        form.addEventListener('submit', addFlickToList)
-        
+        form.addEventListener('submit', (ev) => {
+            ev.preventDefault();
+            this.handleSubmit(ev)
+        })
+    }
+
+    //create a span
+    renderProperty(name, value){
+        const span = document.createElement('span')
+        span.classList.add(name)
+        span.textContent = value
+        return span
+    }
+
+    renderItem(flick){
+        //creates a item list
+        const item = document.createElement('li')
+        item.classList.add('flick')
+    
+        //gets the key for the object
+        const properties = Object.keys(flick);
+    
+        //loops through the whole object and append it to item list
+        properties.forEach((propertyName) => {
+            //creates a span
+            const span = this.renderProperty(propertyName, flick[propertyName])
+            item.appendChild(span);
+        })
+        return item
+    }
+
+    handleSubmit(ev) {
+        const f = ev.target
+
+        //flick Object
+        const flick = {
+            name: f.flickName.value,
+            year: f.flickYear.value,
+        }
+        const item = this.renderItem(flick)
+    
+        const list = document.querySelector('#flicks')
+    
+        list.appendChild(item)
+    
+        f.reset()
     }
 }
 
-
-
-
-
-const handleSubmit = function(ev) {
-    ev.preventDefault()
-    const f = ev.target
-
-    const flick = {
-        name: f.flickName.value,
-        year: f.flickYear.value,
-    }
-
-    const item = renderItem(flick)
-
-    const list = document.querySelector('#flicks')
-
-    list.appendChild(item)
-
-    f.reset()
-}
-
-
-const renderItem = function(flick){
-    //creates a item list
-    const item = document.createElement('li')
-    item.classList.add('flick')
-
-    //gets the key for the object
-    const properties = Object.keys(flick);
-
-    //loops through the whole object and append it to item list
-    properties.forEach(function propertyName() {
-        const span = renderProperty(propertyName, flick[propertyName])
-        item.appendChild(span);
-    })
-
-    return item
-}
-
-//create a span
-const renderProperty = function(name, value){
-    const span = document.createElement('span')
-    span.classList.add(name)
-    span.textContent = value
-    return span
-}
+const app = new App()
